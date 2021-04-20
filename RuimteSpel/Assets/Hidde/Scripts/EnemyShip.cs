c82ac37a870812fe81a6ddc1062eb427213745a6
FireHogGames <hidde01@live.nl>
2021-04-20 15:09:02 +0200

    Enemy health en explosion toegevoegd.

--- RuimteSpel/Assets/Hidde/Scripts/EnemyShip.cs
+++ RuimteSpel/Assets/Hidde/Scripts/EnemyShip.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class EnemyShip : MonoBehaviour
 {
+    [Header("Movement")]
     public bool movement;
     public float thrustSpeed, shootRange;
     public Transform gunOrigin, rotateOrigin;
     public Transform target;
 
+    [Header("Health")]
     public float maxHealth;
     private float health;
-
+    public Rigidbody rb;
+    public GameObject explosion, initialExplosion;
 
     private Vector3 direction;
 
     private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        health = maxHealth;
     }
 
     public void Update()
@@ -39,10 +43,47 @@ public class EnemyShip : MonoBehaviour
             }
         }
 
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(9999f);
+        }
+
+    }
+
+    private void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        movement = false;
+
+        rb.isKinematic = false;
+        rb.AddForce(transform.forward * 1000f);
+
+        GameObject go = Instantiate(initialExplosion, transform.position, transform.rotation);
+
+        Destroy(go, 2f);
+        StartCoroutine(DestroyShip());
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, shootRange);
+    }
+
+    private IEnumerator DestroyShip()
+    {
+        Quaternion rotation = Quaternion.Euler(-90f, 0f, 0f);
+        yield return new WaitForSeconds(3f);
+        GameObject go = Instantiate(explosion, transform.position, rotation);
+        Destroy(go, 10f);
+        Destroy(gameObject);
     }
 }

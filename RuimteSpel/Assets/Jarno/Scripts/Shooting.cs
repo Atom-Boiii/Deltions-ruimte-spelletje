2da2c99849a1f72fed9854f2bf07b85ba3e7eccf
Jarno264 <77965744+Jarno264@users.noreply.github.com>
2021-05-17 02:18:54 +0200

    Shooting + health script

---
+++ RuimteSpel/Assets/Jarno/Scripts/Shooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    
+        [Header("Settings")]
+    [SerializeField] private float _Damage = 20;
+    [SerializeField] private float _ShootDistance = 50;
+    [SerializeField] private Transform _Shootpoint;
+    [SerializeField] private LayerMask _EnemyLayer;
+
+    [Header("Automatic")]
+    [SerializeField] private float _SecondsBetweenShots = 0.5f;
+
+
+    private float _Timer;
+    
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _Timer += 1 * Time.deltaTime;
+             if (_Timer >= _SecondsBetweenShots)
+               {
+                    Shoot();
+                    _Timer = 0;
+               }
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, _Shootpoint.TransformDirection(Vector3.forward), out hit, _ShootDistance, _EnemyLayer))
+                hit.transform.GetComponent<Health>().DoDamage(_Damage);
+    }
+}

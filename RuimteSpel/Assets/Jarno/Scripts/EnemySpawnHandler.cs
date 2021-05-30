b19c2f628de13c315b8ee393a75593a8c0b82901
Jarno264 <77965744+Jarno264@users.noreply.github.com>
2021-05-30 13:49:19 +0200

    EnemySpawnHandler (waveHandler)

---
+++ RuimteSpel/Assets/Jarno/Scripts/EnemySpawnHandler.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnHandler : MonoBehaviour
+{
+    private enum Options { Endless, Waves }
+
+    [Header("Settings")]
+    [SerializeField] private Options Option = Options.Endless;
+    public int Seed = 0;
+    public bool SetRandomSeed = true;
+
+    [Header("Object Pool")]
+    public ObjectPool ObjectPool = null;
+
+    [Header("Enemies")]
+    public EnemySpawnHandler_Enemy[] Enemies = null;
+
+    [Header("SpawnLocations")]
+    public Transform[] SpawnLocations = null;
+
+    [Header("Settings - Endless")]
+    public float SpawnRate = 5; // Seconds between spawning
+    public float SpawnRateEncrease = 0.05f; // Decrease time between spawning per sec
+    public bool RandomEnemy = true;
+    public bool RandomSpawn = true;
+
+    [Header("Settings - Waves")]
+    public EnemySpawnHandler_WaveSettings Waves = null;
+    public bool WaitForAllEnemiesKilled = true;
+
+    private float Timer = 0;
+    private int CurrentWave = 0;
+    private int CheckWave = 999;
+    private float TimerBetweenWaves = 0;
+    private float SpawnSpeed = 0;
+
+    private int _EnemiesAlive = 0;
+
+    private void Start()
+    {
+        if (SetRandomSeed)
+            Random.InitState(Random.Range(0, 10000));
+        else
+            Random.InitState(Seed);
+
+        if (Waves.WaveOption == EnemySpawnHandler_WaveSettings.WaveOptions.Generate)
+            GenerateWaves();
+        if (Waves.WaveOption == EnemySpawnHandler_WaveSettings.WaveOptions.Endless)
+        {
+            Waves.WaveAmount = 1;
+            GenerateWaves();
+            GenerateWaves(1);
+        }
+
+
+    }
+
+    void Update()
+    {
+        Timer += 1 * Time.deltaTime;
+
+        switch (Option)
+        {
+            case Options.Endless:
+                Update_Endless();
+                break;
+            case Options.Waves:
+                Update_Waves();
+                break;
+        }
+    }
+
+    //Update
+    private void Update_Endless()
+    {
+        if (Timer >= SpawnRate)
+        {
+            int randomenemyid = 0;
+            int randomspawnid = 0;
+            if (RandomEnemy)
+                randomenemyid = Random.Range(0, Enemies.Length);
+            if (RandomSpawn)
+                randomspawnid = Random.Range(0, SpawnLocations.Length);
+            Spawn(randomenemyid, randomspawnid);
+            Timer = 0;
+        }
+        SpawnRate -= SpawnRateEncrease * Time.deltaTime;
+    }
+    private void Update_Waves()
+    {
+        if (CurrentWave < Waves.Waves.Count)
+        {
+            if (CheckWave != CurrentWave)
+            {
+                if (WaitForAllEnemiesKilled)
+                {
+                    EnemiesAlive();
+
+                    if (_EnemiesAlive == 0)
+                        TimerBetweenWaves += 1 * Time.deltaTime;
+                }
+                else
+                    TimerBetweenWaves += 1 * Time.deltaTime;
+
+                if (TimerBetweenWaves >= Waves.TimeBetweenWaves)
+                {
+                    TimerBetweenWaves = 0;
+                    CheckWave = CurrentWave;
+                    SpawnSpeed = Waves.Waves[CurrentWave].SpawnDuration / Waves.Waves[CurrentWave].TotalEnemies;
+                    if (Waves.WaveOption == EnemySpawnHandler_WaveSettings.WaveOptions.Endless)
+                        GenerateWaves(CurrentWave + 2);
+                }
+            }
+            else
+            {
+                //Spawn
+                if (Waves.Waves[CurrentWave].TotalEnemies > 0)
+                {
+                    if (Timer > SpawnSpeed)
+                    {
+                        bool spawncheck = false;
+                        while (!spawncheck)
+                        {
+                            int spawnid = Random.Range(0, Enemies.Length);
+                            if (Waves.Waves[CurrentWave].EnemyID[spawnid] > 0)
+                            {
+                                Spawn(spawnid, Random.Range(0, SpawnLocations.Length));
+                                Waves.Waves[CheckWave].EnemyID[spawnid]--;
+                                Waves.Waves[CurrentWave].TotalEnemies--;
+                                spawncheck = true;
+                            }
+                        }
+                        Timer = 0;
+                    }
+                }
+                else
+                {
+                    CurrentWave++;
+                }
+            }
+        }
+    }
+
+    //Generate Waves
+    private void GenerateWaves(int waveid = 0)
+    {
+        int enemytypes = Enemies.Length;
+        for (int i = 0; i < Waves.WaveAmount; i++)
+        {
+            EnemySpawnHandler_Wave newwave = new EnemySpawnHandler_Wave();
+            int enemyamount = 0;
+
+            if (waveid == 0)
+                enemyamount = Mathf.RoundToInt(Waves.EnemyAmount * ((Waves.EnemyIncreaseAmount * i) + 1));
+            else
+                enemyamount = Mathf.RoundToInt(Waves.EnemyAmount * ((Waves.EnemyIncreaseAmount * waveid) + 1));
+
+            //Set enemy amount
+            newwave.EnemyID = new int[enemytypes];
+            int checkenemyamount = 0;
+            newwave.TotalEnemies = enemyamount;
+
+            while (checkenemyamount < enemyamount)
+            {
+                for (int j = 0; j < enemytypes; j++)
+                {
+                    if (Enemies[j].StartWave <= i)
+                    {
+                        int addamount = 0;
+                        if (enemyamount < 2)
+                            addamount = Random.Range(0, enemyamount);
+                        else
+                            addamount = Random.Range(0, Mathf.RoundToInt(enemyamount * 0.5f));
+
+                        if (enemyamount > checkenemyamount + addamount)
+                        {
+                            newwave.EnemyID[j] += addamount;
+                            checkenemyamount += addamount;
+                        }
+                        else
+                        {
+                            newwave.EnemyID[j] += enemyamount - checkenemyamount;
+                            checkenemyamount = enemyamount;
+                            continue;
+                        }
+                    }
+                }
+            }
+            Waves.Waves.Add(newwave);
+        }
+    }
+
+    public void Spawn(int enemyid, int spawnid)
+    {
+        GameObject obj = ObjectPool.GetObjectPrefabName(Enemies[enemyid].EnemyPrefab.name, false);
+        obj.transform.position = SpawnLocations[spawnid].position;
+        obj.SetActive(true);
+    }
+    private void EnemiesAlive()
+    {
+        _EnemiesAlive = GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
+}
+
+[System.Serializable]
+public class EnemySpawnHandler_Enemy
+{
+    public string EnemyName;
+    public GameObject EnemyPrefab;
+
+    [Header("Settings")]
+    public int StartWave;
+}
+
+[System.Serializable]
+public class EnemySpawnHandler_WaveSettings
+{
+    public enum WaveOptions { Endless, Manually, Generate }
+    public WaveOptions WaveOption;
+
+    [Header("Endless")]
+    public float EnemyIncreaseAmount;
+
+    [Header("Manual")]
+    public List<EnemySpawnHandler_Wave> Waves;
+
+    [Header("Generate")]
+    public int WaveAmount;
+    public int EnemyAmount;
+
+    [Header("Other")]
+    public float TimeBetweenWaves;
+}
+
+[System.Serializable]
+public class EnemySpawnHandler_Wave
+{
+    public int[] EnemyID;
+    public float SpawnDuration = 5;
+
+    public int TotalEnemies;
+}
---
+++ RuimteSpel/Assets/Jarno/Scripts/ObjectPool.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public ObjectPool_Pool[] ObjectPools = null;
+
+    private List <Transform> Parents = new List<Transform>();
+
+    private void Awake()
+    {
+        GameObject emptyobject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Destroy(emptyobject.GetComponent<MeshRenderer>());
+        Destroy(emptyobject.GetComponent<BoxCollider>());
+
+        for (int i = 0; i < ObjectPools.Length; i++)
+        {
+            //Create parent
+            GameObject poolparent = Instantiate(emptyobject, transform.position, Quaternion.identity);
+            Destroy(poolparent.GetComponent<MeshRenderer>());
+            Destroy(poolparent.GetComponent<BoxCollider>());
+
+            //Set parent
+            poolparent.transform.parent = transform;
+            poolparent.transform.name = "Pool_" + ObjectPools[i].Name;
+            Parents.Add(poolparent.transform);
+
+            //Create objects
+            for (int o = 0; o < ObjectPools[i].Amount; o++)
+            {
+                GameObject obj = (GameObject)Instantiate(ObjectPools[i].Prefab);
+                obj.transform.parent = poolparent.transform;
+                obj.transform.position = new Vector2(9999, 9999);
+                obj.SetActive(false);
+                ObjectPools[i].Objects.Add(obj);
+            }
+        }
+        Destroy(emptyobject);
+    }
+
+    //GetObject
+    public GameObject GetObject(string objname, bool setactive)
+    {
+        int id = FindObjectPoolID(objname, false);
+        return GetObject(id, setactive);
+    }
+    public GameObject GetObject(GameObject obj, bool setactive)
+    {
+        int id = FindObjectPoolID(obj);
+        return GetObject(id, setactive);
+    }
+    public GameObject GetObjectPrefabName(string prefabname, bool setactive)
+    {
+        int id = FindObjectPoolID(prefabname, true);
+        return GetObject(id, setactive);
+    }
+
+    public GameObject GetObject(int id, bool setactive)
+    {
+        GameObject freeObject = null;
+        bool checkfreeobj = false;
+
+        for (int i = 0; i < ObjectPools[id].Objects.Count; i++)
+        {
+            if (!ObjectPools[id].Objects[i].activeInHierarchy)
+            {
+                ObjectPools[id].Objects[i].transform.position = new Vector3(999, 999, 999);
+                ObjectPools[id].Objects[i].SetActive(setactive);
+                freeObject = ObjectPools[id].Objects[i];
+                return freeObject;
+            }
+        }
+
+        if (!checkfreeobj)
+        {
+            ObjectPools[id].Objects.Clear();
+            freeObject = (GameObject)Instantiate(ObjectPools[id].Prefab, new Vector3(999, 999, 999), Quaternion.identity);
+            freeObject.transform.parent = Parents[id];
+            freeObject.SetActive(setactive);
+            ObjectPools[id].Objects.Add(freeObject);
+            return freeObject;
+        }
+
+        Debug.Log("No Object Found");
+        return null;
+    }
+
+    public List<GameObject> GetAllObjects(GameObject objtype)
+    {
+        int id = FindObjectPoolID(objtype);
+        return ObjectPools[id].Objects;
+    }
+
+    private int FindObjectPoolID(GameObject obj)
+    {
+        int id = 0;
+        for (int i = 0; i < ObjectPools.Length; i++)
+        {
+            if (obj == ObjectPools[i].Prefab)
+            {
+                id = i;
+            }
+        }
+        return id;
+    }
+    private int FindObjectPoolID(string objname, bool isprefab)
+    {
+        for (int i = 0; i < ObjectPools.Length; i++)
+        {
+            if (isprefab)
+                if (objname == ObjectPools[i].Prefab.name)
+                {
+                    return i;
+                }
+                else
+            if (objname == ObjectPools[i].Name)
+                {
+                    return i;
+                }
+        }
+        Debug.Log(objname + " Not Found");
+        return 0;
+    }
+}
+
+[System.Serializable]
+public class ObjectPool_Pool
+{
+    public string Name;
+    public GameObject Prefab;
+    public int Amount;
+    public List<GameObject> Objects;
+}
